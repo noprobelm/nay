1ced53fbd55932775f82ff8b3778d97bd215c953
noprobelm <noprobelm@protonmail.com>
2023-04-14 10:45:07 -0400

    Removed duplicate code from package module

--- nay/package.py
+++ nay/package.py
@@ -9,20 +9,7 @@ import configparser
 import pyalpm
 from pyalpm import Handle
 import requests
-
-
-parser = configparser.ConfigParser(allow_no_value=True)
-parser.read("/etc/pacman.conf")
-
-handle = Handle("/", "/var/lib/pacman")
-
-DATABASES = {
-    db: handle.register_syncdb(db, pyalpm.SIG_DATABASE_OPTIONAL)
-    for db in parser.sections()[1:]
-}
-
-DATABASES["local"] = handle.get_localdb()
-INSTALLED = [pkg.name for pkg in DATABASES["local"].pkgcache]
+from .db import DATABASES, INSTALLED
 
 
 @dataclass(eq=False)
