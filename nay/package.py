791e13a453f05d357f18b4107c647c4ec4fb4264
noprobelm <noprobelm@protonmail.com>
2023-04-29 00:48:47 -0400

    Added rest of pkgcache data to package module

--- nay/package.py
+++ nay/package.py
@@ -15,7 +15,24 @@ from .console import default
 import pyalpm
 from pyalpm import Handle
 
+import configparser
+
+import pyalpm
+from pyalpm import Handle
+from .package import SyncPackage
+
+parser = configparser.ConfigParser(allow_no_value=True)
+parser.read("/etc/pacman.conf")
+
 handle = Handle("/", "/var/lib/pacman")
+DATABASES = {
+    db: handle.register_syncdb(db, pyalpm.SIG_DATABASE_OPTIONAL)
+    for db in parser.sections()[1:]
+}
+
+SYNC_PACKAGES = []
+for db in DATABASES:
+    SYNC_PACKAGES.extend([pkg.name for pkg in DATABASES[db].pkgcache])
 INSTALLED = [pkg for pkg in handle.get_localdb().pkgcache]
 
 
