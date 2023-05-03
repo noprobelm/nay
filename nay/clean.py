109de0028aac212d4986e655a86ec000a646a202
noprobelm <noprobelm@protonmail.com>
2023-05-02 20:49:05 -0400

    Migrated clean and makepkg funcs; updated docs

--- nay/clean.py
+++ nay/clean.py
@@ -1,11 +1,11 @@
-import os
-import shlex
-import shutil
-import subprocess
-from typing import Optional
-from .package import Package
 from .console import console
+import os
+import shutil
+from .package import Package
 from .config import CACHEDIR
+from typing import Optional
+import subprocess
+import shlex
 
 
 def clean_cachedir() -> None:
@@ -65,25 +65,3 @@ def get_pkgbuild(pkg: Package, clonedir: Optional[str] = CACHEDIR, force=False) 
         shlex.split(f"git clone https://aur.archlinux.org/{pkg.name}.git {clonedir}"),
         capture_output=True,
     )
-
-
-def makepkg(pkg: Package, pkgdir, flags: str) -> None:
-    """
-    Make a package using 'makepkg'. This is a pure pacman wrapper.
-
-    :param pkg: The package.Package object to make the package from
-    :type pkg: package.Package
-    :param pkgdir: The full path (exclusive of the package path itself)
-    :type pkgdir: str
-    :param flags: The flags to pass to 'makepkg' (exlusive of the leading '-')
-    :type flags: str
-
-    """
-    os.chdir(f"{pkgdir}/{pkg.name}")
-    return_code = subprocess.run(shlex.split(f"makepkg -{flags}")).returncode
-    if return_code != 0:
-        console.print(f"[red] -> error making: {pkg.name}-exit status {return_code}")
-        console.print(
-            f"[red] Failed to install {pkg.name}. Manual intervention is required"
-        )
-        quit()
