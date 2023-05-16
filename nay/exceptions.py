b965f31a0dfebc248725e7f60774484f48cde309
noprobelm <noprobelm@protonmail.com>
2023-05-16 11:28:37 -0400

    Removed unused argument errors

--- nay/exceptions.py
+++ nay/exceptions.py
@@ -1,12 +1,10 @@
-class ArgumentError(Exception):
-    """Base class for exceptions in this module"""
+class ALPMError(Exception):
+    """Base class for exceptions encountered with libalpm/pyalpm"""
 
     pass
 
 
-class ALPMError(Exception):
-    """Base class for exceptions encountered with libalpm/pyalpm"""
-
+class HandleCreateError(ALPMError):
     pass
 
 
@@ -16,68 +14,5 @@ class ConfigReadError(Exception):
     pass
 
 
-class ConflictingOperations(ArgumentError):
-    """
-    Exception raised when conflicting operations are passed as command line arguments.
-
-    :param message: Explanation of the error
-    :type message: str
-    """
-
-    pass
-
-
-class ConflictingOptions(ArgumentError):
-    """
-    Exception raised when conflicting options are passed as command line arguments.
-
-    :param message: Explanation of the error
-    :type message: str
-    """
-
-    pass
-
-
-class InvalidOperation(ArgumentError):
-    """
-    Exception raised when an invalid operation is passed as a command line argument.
-
-    :param message: Explanation of the error
-    :type message: str
-    """
-
-    pass
-
-
-class InvalidOption(ArgumentError):
-    """
-    Exception raised when invalid options are passed as command line arguments.
-
-    :param message: Explanation of the error
-    :type message: str
-    """
-
-    pass
-
-
-class MissingTargets(ArgumentError):
-    """
-    Exception raised when no targets are passed to an operation requiring arguments.
-
-    :param message: Explanation of the error
-    :type message: str
-    """
-
-
-class PacmanError(ArgumentError):
-    """
-    Exception raised when pacman returns a non-zero return status code
-
-    :param message: Explanation of the error
-    :type message: str
-
-    """
-
-
-class HandleCreateError(ALPMError):
+class MissingTargets(Exception):
     pass
