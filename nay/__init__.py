312ca2e87892432b9df74532428842db076df16c
noprobelm <noprobelm@protonmail.com>
2023-05-24 14:26:40 -0400

    Removed unused imports and funcs

--- nay/__init__.py
+++ nay/__init__.py
@@ -1,34 +1,8 @@
 import os
-from . import wrapper
-from typing import Union
 
 __version__ = "0.3.1"
 
 ROOT_DIR = os.path.dirname(os.path.abspath(__file__))
-
-
-def get_wrappers_mapper():
-    mapper = {
-        "remove": wrapper.Remove,
-        "upgrade": wrapper.Upgrade,
-        "query": wrapper.Query,
-        "database": wrapper.Database,
-        "files": wrapper.Files,
-        "deptest": wrapper.Deptest,
-    }
-    return mapper
-
-
-def get_nay_mapper() -> dict:
-    from . import sync, get_pkgbuild
-
-    mapper = {
-        "sync": sync.Sync,
-        "nay": sync.Nay,
-        "getpkgbuild": get_pkgbuild.GetPKGBUILD,
-    }
-
-    return mapper
 
 
 def get_console(color_system: Union[str, None]) -> "NayConsole":
